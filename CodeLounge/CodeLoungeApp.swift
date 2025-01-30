01078acb517dc06de8e3998468052b8563b8ec18
indextrown <indextrown@gmail.com>
2025-01-30 18:17:59 +0900

    feat: 최신 버전 확인 기능 추가

--- CodeLounge/CodeLoungeApp.swift
+++ CodeLounge/CodeLoungeApp.swift
@@ -13,6 +13,10 @@ struct CodeLoungeApp: App {
     @StateObject var container: DIContainer = DIContainer.init(services: Services())
     @StateObject private var postViewModel = PostViewModel()
     
+    // MARK: - 최신 버전 확인
+    @State private var showUpdateAlert = false
+    @State private var latestVersion: String?
+    
 
     // MARK: - navigationTitle 색상 흰색으로 지정
     init() {
@@ -28,7 +32,59 @@ struct CodeLoungeApp: App {
                 .environmentObject(postViewModel)
                 .onAppear {
                     postViewModel.fetchAllPosts()
+                    checkForAppUpdates()
+                }
+                .alert(isPresented: $showUpdateAlert) {
+                    Alert(
+                        title: Text("앱 업데이트 필요"),
+                        message: Text("새로운 기능과 성능 개선을 위해 최신 버전 (\(latestVersion ?? ""))을 사용해 보세요!"),
+                        dismissButton: .default(Text("업데이트")) {
+                            if let url = URL(string: "https://apps.apple.com/app/id6741165577") {
+                                UIApplication.shared.open(url)
+                            }
+                        }
+                    )
                 }
         }
+    }
+    
+    // 최신 버전 확인 로직
+    func checkForAppUpdates() {
+        let currentVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "0.0.0"
+        
+        fetchLatestVersionFromAppStore { latest in
+            if let latest = latest, isUpdateRequired(currentVersion: currentVersion, latestVersion: latest) {
+                self.latestVersion = latest
+                self.showUpdateAlert = true
+            }
+        }
+    }
+    
+    // iTunes API에서 최신 버전 가져오기
+    func fetchLatestVersionFromAppStore(completion: @escaping (String?) -> Void) {
+        guard let url = URL(string: "https://itunes.apple.com/lookup?bundleId=com.indextrown.CodeLounge") else { return }
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else {
+                completion(nil)
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
+                   let results = json["results"] as? [[String: Any]],
+                   let latestVersion = results.first?["version"] as? String {
+                    completion(latestVersion)
+                } else {
+                    completion(nil)
+                }
+            } catch {
+                completion(nil)
+            }
+        }.resume()
+    }
+    
+    // 버전 비교 로직
+    func isUpdateRequired(currentVersion: String, latestVersion: String) -> Bool {
+        return currentVersion.compare(latestVersion, options: .numeric) == .orderedAscending
     }
 }
